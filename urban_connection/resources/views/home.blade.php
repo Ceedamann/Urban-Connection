5b8c5b75f7aa822c9eab155215daf3e709b1d71b
lethalSid <senien.hicks@gmail.com>
2019-11-04 11:20:40 -0500

    fixed app page

--- urban_connection/resources/views/home.blade.php
+++ urban_connection/resources/views/home.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
-<!-- @section('content')
-{{-- <div class="container">
+@section('content')
+<div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        {{-- <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
@@ -16,7 +16,59 @@
                     You are logged in!
                 </div>
             </div>
+        </div> --}}
+    </div>
+</div>
+
+<main class="py-4">
+        @auth
+        <div class="container">
+        @if(session()->has('success'))
+            <div class="alert alert-success">
+                {{session()->get('success')}}
+             </div>
+        @endif
+        <div class="row">
+            <div class="col-md-4">
+                <ul class="list-group">
+                     <li class="list-group-item">
+                         <a href="{{route('posts.index')}}">Posts</a>
+
+                     </li>
+                     <li class="list-group-item">
+                         <a href="{{route('categories.index')}}">Categories</a>
+
+                     </li>
+                     <li class="list-group-item">
+                         <a href="{{route('tags.index')}}">Tags</a>
+
+                     </li>
+                </ul>
+                <ul class="list-group mt-5">
+                     <li class="list-group-item">
+                         <a href="{{route('trashed-posts.index')}}">Trashed posts</a>
+                     </li>
+                </ul>
+             </div>
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        You are logged in!
+                    </div>
+                </div>
+                @yield('content1')
+            </div>
         </div>
     </div>
-</div> --}}
-@endsection -->
+        @else
+           @yield('content1')
+        @endauth
+    </main>
+@endsection
