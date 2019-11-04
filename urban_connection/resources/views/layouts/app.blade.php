6031284120ec7247ec0dadf3bfd8df3c81f32d0b
lethalSid <senien.hicks@gmail.com>
2019-11-04 11:26:33 -0500

    erased not needed commmented code

--- urban_connection/resources/views/layouts/app.blade.php
+++ urban_connection/resources/views/layouts/app.blade.php
@@ -29,45 +29,6 @@
             @include('inc.navbar')
             @yield('content')
         </div>
-        {{-- <main class="py-4">
-           @auth
-           <div class="container">
-           @if(session()->has('success'))
-               <div class="alert alert-success">
-                   {{session()->get('success')}}
-                </div>
-           @endif
-           <div class="row">
-               <div class="col-md-4">
-                   <ul class="list-group">
-                        <li class="list-group-item">
-                            <a href="{{route('posts.index')}}">Posts</a>
-
-                        </li>
-                        <li class="list-group-item">
-                            <a href="{{route('categories.index')}}">Categories</a>
-
-                        </li>
-                        <li class="list-group-item">
-                            <a href="{{route('tags.index')}}">Tags</a>
-
-                        </li>
-                   </ul>
-                   <ul class="list-group mt-5">
-                        <li class="list-group-item">
-                            <a href="{{route('trashed-posts.index')}}">Trashed posts</a>
-                        </li>
-                   </ul>
-                </div>
-               <div class="col-md-8">
-                   @yield('content1')
-               </div>
-           </div>
-       </div>
-           @else
-              @yield('content1')
-           @endauth
-       </main> --}}
    </div>
 
     <!-- Scripts -->
