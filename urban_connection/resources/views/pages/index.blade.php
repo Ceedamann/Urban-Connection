7f3d54e8f40fb6b493bc6e430ba14450fbe11a09
lethalSid <senien.hicks@gmail.com>
2019-11-03 19:12:47 -0500

    fixed navbar for landing page and dashboard page

--- urban_connection/resources/views/pages/index.blade.php
+++ urban_connection/resources/views/pages/index.blade.php
@@ -23,7 +23,15 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">Dashboard</a>
+                        <a href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                      document.getElementById('logout-form').submit();">
+                         {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
