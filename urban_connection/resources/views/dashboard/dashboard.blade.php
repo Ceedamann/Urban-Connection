cf22bf9d436e3b7a08706723d02d6f01e604a00d
lethalSid <senien.hicks@gmail.com>
2019-11-03 23:03:09 -0500

    fixed navbar issues on app page need to address styling

--- urban_connection/resources/views/dashboard/dashboard.blade.php
+++ urban_connection/resources/views/dashboard/dashboard.blade.php
@@ -1,38 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Urban Connection') }}</title>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css' integrity='sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO' crossorigin='anonymous'>
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-
-    <link href="{{ asset('css/events.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/about.css') }}" rel="stylesheet">
-    @stack('scripts')
-    @yield("css")
-</head>
-    <body>
-        <div id="app">
-            <div class="container">
-                @include('inc.navbar')
-                @include('dashboard.dashboard')
-
-                @yield('content')
-
-
-
-            {{-- <div class="container">
+ <div class="container">
                 <main class="py-4">
                 @auth
                     <div class="container">
@@ -63,14 +29,8 @@
                         </div>
                     </div>
                 @else
+                @yield('content')
                 </div>
                 </main>
                 @endauth
-            </div> --}}
-        </div>
-        </div>
-    <!-- Scripts -->
-    @yield('scripts')
-    <script src="{{ asset('js/app.js') }}"></script>
-    </body>
-</html>
+</div>
