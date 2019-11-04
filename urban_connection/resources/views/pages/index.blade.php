e88d55540a759558148a986612548f107023a851
lethalSid <senien.hicks@gmail.com>
2019-11-03 19:34:45 -0500

    extended navbar to events page and also extended css styles

--- urban_connection/resources/views/pages/index.blade.php
+++ urban_connection/resources/views/pages/index.blade.php
@@ -13,7 +13,7 @@
 
 
         <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
     </head>
     <body>
         <video autoplay muted loop id="myVideo">
