d5b86f6983423a8a3fa33a47433cf01c8a22fcc7
lethalSid <senien.hicks@gmail.com>
2019-11-19 00:21:32 -0500

    errased foot tags and rebuilt from gound up

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -5,25 +5,18 @@
 
 
 <!-- Footer -->
-    {{-- <footer class="footer">
-        <div class="container">
-          <div class="row gap-y align-items-center">
+    <div class="footers">
+      <div class="container">
+        <a href="/">Urban Connection | </a>
+        <a href="/about"> &copy; 2019</a>
+      </div>
+    </div>
 
-            <div class="col-6 col-lg-3">
-                <div class="row">
-                    <a href="/">Urban Connection</a>
-                    <a href="/about">@ Copyright 2019</a>
-                </div>
-            </div>
-          </div>
-        </div>
-      </footer> --}}
-
-      <footer class="footer sticky mt-auto py-3 row gap-y align-items-center">
+      {{-- <footer class="footer sticky mt-auto py-3 row gap-y align-items-center">
             <div class="container">
                 <span class="text-muted"><a style="color:white;" href="/">Urban Connection</a></span>
-                <span class="text-muted" ><a style="color:white;" href="/about">@ Copyright 2019</a></span>
+                <span class="text-muted" ><a style="color:white;" href="/about">Copyright 2019</a></span>
             </div>
-        </footer>
+        </footer> --}}
 
       <!-- /.footer -->
